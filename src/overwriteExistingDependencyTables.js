b7982690591d18a686b970a2d12dadcdc94a0d9c
Nick Krantz <krantni@gmail.com>
2021-10-29 23:22:22 -0500

    fixing overwriting existing dependency tables

--- src/overwriteExistingDependencyTables.js
+++ src/overwriteExistingDependencyTables.js
@@ -2,47 +2,62 @@ const fs = require('fs');
 const readline = require('readline');
 const constants = require('./constants');
 const utils = require('./utils');
+const readFileByLine = require('./readFileByLine');
 
 /**
- * Inserts new Runtime & Development table, overwriting the old tables
+ * Inserts new Runtime & Development table, overwriting the old tables.
+ * Old table rows are processed but not added to the `newFile`.
  * 
- * @param {*} runtimeTable runtime dependency markdown table
- * @param {*} devTable development dependency markdown table 
+ * @param {string} runtimeTable runtime dependency markdown table
+ * @param {string} devTable development dependency markdown table 
  */
 async function overwriteExistingDependencyTables(runtimeTable, devTable) {
-  const fileStream = fs.createReadStream('./README.md');
-  let header;
-  let foundTableHeader = false;
-  let firstNewLineSkipped = false;
+  let skipOldTableRows = false;
+  let numberOfEmptyLines = 0;
   let newFile = '';
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  for await (const line of rl) {
-    // TODO FIX THIS LOGIC
+  await readFileByLine('./README.md', (line) => {
+    // When a line is a dependency heading, insert the corresponding table beneath it.
     if (utils.isLineAHeading(line)) {
-      foundTableHeader = true;
-      header = line === constants.RUNTIME_DEPENDENCY_HEADING ? 'runtime' : 'dev';
-      newFile += `${line}\n\n`
-      if (header === 'runtime') {
-        newFile += `${runtimeTable}\n`
+      skipOldTableRows = true;
+      numberOfEmptyLines = 0;
+
+      // Add heading to new file
+      newFile += `${line}\n\n`;
+
+      // Add the corresponding dependency table to new file string
+      if (line === constants.RUNTIME_DEPENDENCY_HEADING) {
+        newFile += `${runtimeTable}\n`;
       } else {
-        newFile += `${devTable}\n`
+        newFile += `${devTable}\n`;
       }
-      continue;
+
+      // Skip to next line
+      return;
     }
-    if (line.split('').filter(Boolean).length === 0) {
-      firstNewLineSkipped = true;
-      continue;
+
+    // When skipping old table rows
+    if (skipOldTableRows) {
+
+      // If there have been 2 empty lines, that assumes the end of the table
+      // Reset tracking variables
+      if (numberOfEmptyLines >= 2) {
+        numberOfEmptyLines = 0;
+        skipOldTableRows = false;
+        newFile += `${line}\n`;
+        return
+      }
+
+      // If the line is empty, increase the counter
+      if (line.split('').filter(Boolean).length === 0) {
+        numberOfEmptyLines++;
+        return;
+      }
+    } else {
+      // Add line to file
+      newFile += `${line}\n`;
     }
-    if (foundTableHeader && firstNewLineSkipped && !line.split('').filter(Boolean).length) {
-      foundTableHeader = false;
-      firstNewLineSkipped = false;
-    }
-  }
+  })
 
   await fs.writeFileSync('./README.md', newFile)
 }
